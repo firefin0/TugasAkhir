469c06fe63543f714ad874ea18abea92ab96b518
fikri anggawie <47374271+firefin0@users.noreply.github.com>
2021-11-24 10:04:15 +0700

    edit ver 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('selamat pagi mencoba');
+  res.end('selamat pagi mencoba terus menerus');
 });
 
 server.listen(port, hostname, () => {
